13cbc373358d7cc69dca9c72173d892780c662d7
janjunyoung123 <hjy961017@gmail.com>
2024-09-10 16:53:59 +0900

    패밀리부분수정완료 가족여행부분 반응형 수정중

--- js/swiper.js
+++ js/swiper.js
@@ -3,14 +3,28 @@ window.addEventListener("load", function () {
     slidesPerView: 2.3,
     spaceBetween: 30,
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".swiper-button-nextt",
+      prevEl: ".swiper-button-prevt",
+    },breakpoints:{
+      1420 : {
+        slidesPerView: 2.3,
+      },
+      768 : {
+        slidesPerView: 2.3,
+      },
+      350 : {
+        slidesPerView: 2,
+      },
+      320 : {
+        slidesPerView: 1,
+      }
     },
     loop: true,
   });
   var bannerswiper = new Swiper(".familySwiper", {
     slidesPerView: 2.3,
     spaceBetween: 30,
+    
     navigation: {
       nextEl: ".swiper-button-nextt",
       prevEl: ".swiper-button-prevt",
