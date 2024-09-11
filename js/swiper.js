21133c6208e1c40801c75ccfcbe4c4a59a563ad0
janjunyoung123 <hjy961017@gmail.com>
2024-09-11 11:38:45 +0900

    반응형

--- js/swiper.js
+++ js/swiper.js
@@ -23,11 +23,23 @@ window.addEventListener("load", function () {
   });
   var bannerswiper = new Swiper(".familySwiper", {
     slidesPerView: 2.3,
-    spaceBetween: 30,
-    
+    spaceBetween: 30,  
     navigation: {
       nextEl: ".swiper-button-nextt",
       prevEl: ".swiper-button-prevt",
+    },breakpoints:{
+      1420 : {
+        slidesPerView: 2.3,
+      },
+      768 : {
+        slidesPerView: 2.3,
+      },
+      350 : {
+        slidesPerView: 2,
+      },
+      320 : {
+        slidesPerView: 1,
+      }
     },
     loop: true,
   });
